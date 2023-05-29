c2f9ea03b04beb46ae2f52ae794915aa64d65dfb
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-05-29 14:00:04 +0700

    practice_create_a_custom_widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: const [
           Color.fromARGB(255, 26, 2, 80),
           Color.fromARGB(255, 45, 7, 98),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ], begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
+        child: StyledText(),
       ),
     );
   }
