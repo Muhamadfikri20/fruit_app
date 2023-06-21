d923b1b92ddea7f7aca3e8fad4bb873a4ebf2b25
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-06-21 14:30:30 +0700

    practice_reusable_widgets_and_constructor_functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:fruit_app/gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(body: GradientContainer(Color.fromARGB(255, 33, 5, 109), Color.fromARGB(255, 68, 21, 149))),
     ),
   );
 }
