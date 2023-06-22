990b5f03caf76d24ecd93c2b47f894f19953ebcb
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-06-22 13:41:06 +0700

    displaying_images_and_using_multiple_constructor_functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fruit_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
   final Color color1;
   final Color color2;
   @override
@@ -14,8 +16,11 @@ class GradientContainer extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
-      child: const Center(
-        child: StyledText("Hello World"),
+      child: Center(
+        child: Image.asset(
+          'assets/images/fruit-1.png',
+          width: 200,
+        ),
       ),
     );
   }
